f367748a0d36e146e5d970e286e4b675e6bf2690
Josh Newsom <newsommeister@gmail.com>
2016-11-19 22:18:29 -0700

    Ignore .sass-cache

--- client/js/controllers/chores.controller.js
+++ client/js/controllers/chores.controller.js
@@ -6,6 +6,8 @@ function ChoresCtrl($scope, $http, ChoresService, DateService) {
     var vm = this;
 
     vm.today = DateService.today;
+    vm.todaysDate = DateService.todaysDate;
+    vm.monthName = DateService.monthName;
     ChoresService.getChores
         .then(function(chores) {
             vm.chores = chores;
--- client/js/services/chores.service.js
+++ client/js/services/chores.service.js
@@ -12,7 +12,7 @@ function ChoresService($http, DateService) {
     });
 
     function finish(chore) {
-      
+
     }
 
     return {
